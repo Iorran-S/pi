d3337a5e2295ad75f330fcbd6d7b7b836d9c9a2e
iorran <iorransa@hotmail.com>
2024-05-18 12:08:09 -0300

    Ajuste nos jpg

--- pi/settings.py
+++ pi/settings.py
@@ -23,6 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Secret key e debug mode
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
@@ -110,15 +113,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app_de_cadastro', 'static'),
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
